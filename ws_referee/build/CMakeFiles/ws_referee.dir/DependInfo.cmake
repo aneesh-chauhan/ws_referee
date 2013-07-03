e78838423c0afba45534ed3c374f4a5aa834cab4
aneesh <aneesh.chauhan@ua.pt>
2013-07-03 13:06:21 +0100

    firs commit for the referee

---
+++ ws_referee/build/CMakeFiles/ws_referee.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aneesh/ros_fuerte/RACE_TESTS/ws_referee/src/main.cpp" "/home/aneesh/ros_fuerte/RACE_TESTS/ws_referee/build/CMakeFiles/ws_referee.dir/src/main.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ ws_referee/build/CMakeFiles/ws_referee.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ws_referee.dir/src/main.o"
+  "../bin/ws_referee.pdb"
+  "../bin/ws_referee"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ws_referee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
